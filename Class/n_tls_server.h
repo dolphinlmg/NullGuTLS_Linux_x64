2bc71f8d7964e8995dca631a8458eb1fee827315
dolphinlmg <dolphinlmg@gmail.com>
2019-10-17 22:26:15 +0900

    Update server class

--- Class/n_tls_server.h
+++ Class/n_tls_server.h
@@ -1,19 +1,29 @@
 #ifndef N_TLS_SERVER_H
 #define N_TLS_SERVER_H
 #include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <iostream>
+#include <unistd.h>
+#include <memory.h>
 
 class n_TLS_Server
 {
 public:
-    n_TLS_Server(int port);
+    n_TLS_Server(unsigned short port);
     bool openSocket();
     bool bindSocket();
-    bool setListen();
-    void setPort(int port);
-    int getPort() const;
+    bool setListen(int count = 1);
+    void setPort(unsigned short port);
+    bool acceptSocket();
+    bool openServer();
+    unsigned short getPort() const;
 private:
-    struct sockaddr sock;
-    int fd;
+    struct sockaddr_in server_addr;
+    struct sockaddr_in client_addr;
+    int server_sock;
+    int client_sock;
+    socklen_t addr_size;
 };
 
 #endif // N_TLS_SERVER_H
