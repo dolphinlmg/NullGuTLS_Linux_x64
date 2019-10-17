e65d544a2e2ee4f44a71f78324caff1ab1ff88a8
dolphinlmg <dolphinlmg@gmail.com>
2019-10-17 22:25:53 +0900

    Update server class

--- Class/n_tls_server.cpp
+++ Class/n_tls_server.cpp
@@ -8,8 +8,8 @@ n_TLS_Server::n_TLS_Server(unsigned short port) {
 }
 
 bool n_TLS_Server::openSocket() {
-    fd = socket(PF_INET, SOCK_STREAM, 0);
-    if (fd == -1) {
+    server_sock = socket(PF_INET, SOCK_STREAM, 0);
+    if (server_sock == -1) {
         std::cerr << "Server socket create fail" << std::endl;
         return false;
     }
@@ -17,14 +17,15 @@ bool n_TLS_Server::openSocket() {
 }
 
 bool n_TLS_Server::bindSocket() {
-    if (fd == -1)	return false;
-    if (bind(fd, reinterpret_cast<struct sockaddr*>(&server_addr), sizeof(sockaddr_in)) == -1)
+    if (server_sock == -1)	return false;
+    if (bind(server_sock, reinterpret_cast<struct sockaddr*>(&server_addr), sizeof(server_addr)) == -1)
         return false;
     return true;
 }
 
 bool n_TLS_Server::setListen(int count) {
-    if (listen(fd, count) == -1)
+    if (server_sock == -1) return false;
+    if (listen(server_sock, count) == -1)
         return false;
     return true;
 }
@@ -33,6 +34,23 @@ void n_TLS_Server::setPort(unsigned short port) {
     this->server_addr.sin_port = htons(port);
 }
 
+bool n_TLS_Server::acceptSocket() {
+    if (server_sock == -1) return false;
+    this->addr_size = sizeof(client_addr);
+    this->client_sock = accept(server_sock, reinterpret_cast<struct sockaddr*>(&client_addr), &this->addr_size);
+    if (this->client_sock == -1) return false;
+    write(this->client_sock, "Server Hello", 12);
+    return true;
+}
+
 unsigned short n_TLS_Server::getPort() const {
     return ntohs(this->server_addr.sin_port);
+}
+
+bool n_TLS_Server::openServer() {
+    bool ret = this->openSocket() |
+            this->bindSocket() |
+            this->setListen() |
+            this->acceptSocket();
+    return ret;
 }
