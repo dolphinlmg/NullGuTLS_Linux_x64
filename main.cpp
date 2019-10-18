207232e76b2f5391ac326aa137a3c0a0f30f388b
dolphinlmg <dolphinlmg@gmail.com>
2019-10-18 18:41:41 +0900

    Update test server

--- main.cpp
+++ main.cpp
@@ -1,11 +1,38 @@
 #include <iostream>
 #include "Class/n_tls_server.h"
+#define MAX_SIZE 1024
 
 using namespace std;
 
 int main()
 {
     n_TLS_Server server(1234);
-    server.openTestServer();
-    return 0;
+    char receve_buf[MAX_SIZE] = "";
+
+    server.openSocket();
+    server.bindSocket();
+    server.setListen();
+
+    while(true) {
+        if (!server.acceptSocket()) return -1;
+        cout << "Peer connected" << endl;
+        int fd = server.getClientSock();
+        cout << "fd: " << fd << endl;
+        while(true) {
+            cout << "reading from fd: " << fd << endl;
+            long status = read(fd, receve_buf, MAX_SIZE);
+            cout << "read bytes are: " << status << endl;
+            if (status > 0) {
+                receve_buf[status] = 0;
+                cout << receve_buf << endl << endl;
+                write(fd, "Server receved", 15);
+            } else if (status == 0) {
+                cout << "Peer close the socket.." << endl << endl;
+                close(fd);
+                break;
+            }
+            memset(receve_buf, 0, MAX_SIZE);
+        }
+    }
+
 }
